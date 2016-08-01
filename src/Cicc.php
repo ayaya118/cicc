1bf630bdcc61908f352cf260dfe42664bf972c42
wu cheng <ayaya118@163.com>
2016-08-01 16:20:31 +0800

    cicc

---
+++ src/Cicc.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wucheng
+ * Date: 2016/8/1
+ * Time: 16:14
+ */
+
+namespace Cicc;
+
+
+class Cicc
+{
+    public static function tx1312(){
+        exit("tx1312");
+    }
+}
