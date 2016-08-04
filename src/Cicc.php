bca510352da73aacc69cdc3d025bdf23dce13a93
wu cheng <ayaya118@163.com>
2016-08-04 15:39:39 +0800

    cicc

--- src/Cicc.php
+++ src/Cicc.php
@@ -42,6 +42,16 @@ class Cicc
     }
 
     /**
+     * 接受支付状态通知
+     * @author wucheng
+     * @param $psot
+     * @return int
+     */
+    public function tx1312notice_background($psot){
+        return $this->payment->tx1312process_notice_background($psot);
+    }
+
+    /**
      * 主动查询支付状态
      * @author wucheng
      * @param $institution_id
